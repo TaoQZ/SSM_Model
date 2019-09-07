749db0a2fefc826f4c0d44680c463d8ce51b963f
TaoQZ <2538474354qq.com>
2019-09-07 21:26:10 +0800

    update SpringMvcConfig

--- src/main/java/com/czxy/config/SpringMVConfig.java
+++ src/main/java/com/czxy/config/SpringMVConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.annotation.Resource;
@@ -27,6 +28,9 @@ public class SpringMVConfig extends WebMvcConfigurerAdapter {
     @Resource
     private MyInterceptor2 myInterceptor2;
 
+    /**
+     * controller 的返回的请求路径的前后缀
+     */
 //    @Bean
 //    public InternalResourceViewResolver internalResourceViewResolver(){
 //        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -35,12 +39,30 @@ public class SpringMVConfig extends WebMvcConfigurerAdapter {
 //        return viewResolver;
 //    }
 
+    /**
+     * 添加拦截器
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        InterceptorRegistration interceptorRegistration1 = registry.addInterceptor(myInterceptor1);
 //        InterceptorRegistration interceptorRegistration2 = registry.addInterceptor(myInterceptor2);
 //        interceptorRegistration1.addPathPatterns("/**");
 //        interceptorRegistration2.addPathPatterns("/**");
+
+//        registry.addInterceptor(myInterceptor).addPathPatterns("/**").excludePathPatterns("/**/*.js","/**/*.css","/**/*.png","/**/*.jpg","/**/font/**");
+
+    }
+
+    /**
+     * 虚拟路径映射
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("添加映射路径");
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:D:/temp/upload/");
+//        registry.addResourceHandler("/upload/**").addResourceLocations("file:/upload/");
     }
 
 
